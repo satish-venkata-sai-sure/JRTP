e75e70e38f22796ee49b455c9a249cd6674964e9
Satish <sure.saisatish@gmail.com>
2022-01-30 19:42:32 +0530

    my first commit

---
+++ src/main/java/com/satishit/_Maven_App/App.java
@@ -0,0 +1,13 @@
+package com.satishit._Maven_App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
